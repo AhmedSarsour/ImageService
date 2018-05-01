c41bdd4c0a9c78a66401821a002ed2bcdc9a3731
eliad1998 <eliad98@gmail.com>
2018-05-01 14:49:26 +0300

    Adding some files

--- ImageServiceGui/ImageServiceGui/View/SettingsView.xaml.cs
+++ ImageServiceGui/ImageServiceGui/View/SettingsView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ImageServiceGui
+namespace ImageServiceGui.View
 {
     /// <summary>
     /// Interaction logic for SettingsView.xaml
---
+++ ImageServiceGui/ImageServiceGui/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGui.ViewModel
+{
+    class SettingsViewModel:INotifyPropertyChanged
+    {
+        #region Notify Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        //The properties we want to bind into
+        public string outPutFolder { get; set; }
+        public string sourceName { get; set; }
+        public string logName { get; set; }
+        public string ThumbnailSize { get; set; }
+
+    }
+}
