760574cfc9ece57a70880d5503bfa45a58c5e0ce
eliad1998 <eliad98@gmail.com>
2018-06-04 18:55:25 +0300

    Almost finishing with the main page

--- ImageService.WebApplication/Controllers/FirstController.cs
+++ ImageService.WebApplication/Controllers/FirstController.cs
@@ -6,24 +6,22 @@ using System.Web;
 using System.Web.Mvc;
 using ImageService.WebApplication.Models;
 using System.IO;
+using System.Web.Hosting;
 
 namespace ImageService.WebApplication.Controllers
 {
     public class FirstController : Controller
     {
-
-     
-    
         private static List<Student> ReadStudentsFile()
         {
             List<Student> students = new List<Student>();
           //  string dir = Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory());
 
-            string directory = @"Files/Students.txt";
+            string path = HostingEnvironment.MapPath("~/App_Data/Students.txt");
 
             try
             {
-                string[] lines = System.IO.File.ReadAllLines(directory);
+                string[] lines = System.IO.File.ReadAllLines(path);
                 foreach (string line in lines)
                 {
                     students.Add(new Student(line));
@@ -37,73 +35,17 @@ namespace ImageService.WebApplication.Controllers
 
         static List<Student> students = ReadStudentsFile();
 
-
-[HttpGet]
-        public JObject GetEmployee()
-        {
-            JObject data = new JObject();
-            data["FirstName"] = "Kuky";
-            data["LastName"] = "Mopy";
-            return data;
-        }
-
-
         // GET: First/ImageWeb
         public ActionResult ImageWeb()
         {
-            return View(students);
+            ImageWebModel model = new ImageWebModel();
+            model.NumPhotos = 20;
+            model.Students = students;
+            return View(model);
         }
+
 
     
 
-        // GET: First/Edit/5
-        public ActionResult Edit(int id)
-        {
-            foreach (Student emp in students) {
-                if (emp.ID.Equals(id)) { 
-                    return View(emp);
-                }
-            }
-            return View("Error");
-        }
-
-        // POST: First/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, Student empT)
-        {
-            try
-            {
-                foreach (Student st in students)
-                {
-                    if (st.ID.Equals(id))
-                    {
-                        st.copy(empT);
-                        return RedirectToAction("Photos");
-                    }
-                }
-
-                return RedirectToAction("Photos");
-            }
-            catch
-            {
-                return RedirectToAction("Error");
-            }
-        }
-
-        // GET: First/Delete/5
-        public ActionResult Delete(int id)
-        {
-            int i = 0;
-            foreach (Student emp in students)
-            {
-                if (emp.ID.Equals(id))
-                {
-                    students.RemoveAt(i);
-                    return RedirectToAction("ImageWeb");
-                }
-                i++;
-            }
-            return RedirectToAction("Error");
-        }
     }
 }
---
+++ ImageService.WebApplication/Models/ImageWebModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ImageService.Communication;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImageService.WebApplication.Models
+{
+    public class ImageWebModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Service status")]
+        public string Status { get;  private set; }
+        [Required]
+        [Display(Name = "Number of photos")]
+        public int NumPhotos { get; set; }
+        //A student that we can't set but we use it only for the display names
+        public Student StudentInfo { get; private set; }
+
+        public ImageWebModel()
+        {
+            //Check if the tcp client is connected
+            if (TcpClientChannel.connected)
+            {
+                Status = "Connected";
+            }
+
+            else
+            {
+                Status = "Not connected";
+            }
+        }
+
+    }
+}
